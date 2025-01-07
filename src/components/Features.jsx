599688f97910b3c6637d72a96556e93b90c73817
Ashishroy1724 <ashishroy78782@gmail.com>
2025-01-07 11:31:57 +0530

    Final Touch

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -67,10 +67,6 @@ export const BentoCard = ({ src, title, description, VisitSite, link }) => {
         src={src}
         alt={title}
         className="absolute left-0 top-0 size-full object-cover object-center"
-        autoPlay
-        muted
-        loop
-        playsInline
       />
       <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
         <div>
